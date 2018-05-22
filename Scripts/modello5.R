305ad140f14e2787f7f4f81082fbfa3ea7ee6e47
mrepetto94 <mrepetto94@me.com>
2018-05-22 08:07:55 +0200

    Some changes

--- Scripts/modello5.R
+++ Scripts/modello5.R
@@ -22,12 +22,6 @@ monthlyHour <- c(170,170)
 
 presence <- matrix(TRUE, nrow = nw, ncol=(nt*nd),byrow=TRUE)
 
-columnNames<- c("Worker", "WeeklyHour", "MonthlyHour",apply(cbind(sort(rep(days,3)), Type), 1, function(x) paste(sort(x), collapse=".")))
-
-df <- data.frame(Worker, weeklyHour, monthlyHour, presence)
-
-names(df) <- columnNames
-
 ncol <- (nt*nd*nw)+
 	(nw)
 
@@ -56,7 +50,8 @@ for (q in 1: nw){
 
 #creates the daily hour constraint
 replace <- rep(c((Hour),rep(0,n-nh)),nw)
-replace <-lagmatrix(replace, 6)[,c(1,4,7)] #to be adjusted with the days
+
+replace <-lagmatrix(replace, nt*nd)[,c(1,(1+(1*nt)),(1+(2*nt)))] #to be adjusted with the days
 replace[is.na(replace)] <- 0
 matobj <- matrix(0L, ncol = nd, nrow = nw)
 replace <- t(rbind(replace, matobj))
@@ -64,28 +59,28 @@ replace <- t(rbind(replace, matobj))
 mat <- rbind(mat, replace)
 
 #creates the schedule type constraints
-replace <- rep(c(1,rep(0,n-1)),nw)
-replace <-lagmatrix(replace, n-1) #to be adjusted with the days
-replace[is.na(replace)] <- 0
-matobj <- matrix(0L, ncol = n, nrow = nw)
-replace <- t(rbind(replace, matobj))
+#replace <- rep(c(1,rep(0,n-1)),nw)
+#replace <-lagmatrix(replace, n-1) #to be adjusted with the days
+#replace[is.na(replace)] <- 0
+#matobj <- matrix(0L, ncol = n, nrow = nw)
+#replace <- t(rbind(replace, matobj))
 
-mat <- rbind(mat, replace)
+#mat <- rbind(mat, replace)
 
 
 dir <- c(rep("==",nw),
-	 rep(">=",nd),
-	 rep(">=",n))
+	 rep(">=",nd))
+#	 rep(">=",n))
 
 type <- c(rep("B",n*nw),
 	rep("C", nw))
 
 rhs <- c(rep(0,nw),
-	 rep(36,nd),
-	 rep(1,n))
+	 rep(36,nd))
+#	 rep(1,n))
 
-bounds <- list(lower = list(ind = 1:ncol, val = rep(0,ncol)),
-	       upper = list(ind = (((n*nw)+1):ncol), monthlyHour))
+#bounds <- list(lower = list(ind = 1:ncol, val = rep(0,ncol)),
+#	       upper = list(ind = (((n*nw)+1):ncol), monthlyHour))
 
 system <- data.frame(mat,dir,rhs)
 
@@ -93,8 +88,10 @@ sol <- Rglpk_solve_LP(obj,mat,dir,rhs,types=type)
 
 solution  <- sol$solution[1:(length(sol$solution)-nw)]
 solution <- matrix(solution, nrow=nw,ncol=nt*nd, byrow=TRUE)
-df <- data.frame(Worker,solution)
-colnames(df)<-c("Worker", rep(Type,nd))
+df <- data.frame(solution)
+colnames(df)<-rep(Type,nd)
+rownames(df)<-Worker
+t(df)
 
 write.csv2(system,"system.csv")
 
